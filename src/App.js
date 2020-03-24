cc22668443814ac266c1769898da0af41ee9a8dc
Hesa Muhammad <hesamilan@gmail.com>
2020-03-24 11:27:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -11,58 +11,56 @@ import MainPage from "./pages/MainPage";
 import auth from "./Auth";
 
 function App() {
-  let history = useHistory();
+    let history = useHistory();
 
-  useEffect(() => {
-    if (localStorage.getItem("codeoToken")) {
-      auth.login(() => {
-        history.push("/dashboard");
-      });
-    } else {
-      auth.logout(() => {
-        history.push("/");
-      });
-    }
-  }, []);
+    useEffect(() => {
+        if (localStorage.getItem("codeoToken")) {
+            auth.login(() => {
+                history.push("/dashboard");
+            });
+        } else {
+            auth.logout(() => {
+                history.push("/");
+            });
+        }
+    }, []);
 
-  return (
-    <>
-      
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="/dashboard" component={MainPage} />
-        <Route path="/userManagement" component={MainPage} />
-        <Route path="/cmsUpdate" component={MainPage} />
-        <Route path="/ledger" component={MainPage} />
-        <Route path="/kycApproval" component={MainPage} />
-      </Switch>
-
-    </>
-  );
+    return (
+        <>
+            <Switch>
+                <Route exact path="/" component={Login} />
+                <Route path="/dashboard" component={MainPage} />
+                <Route path="/userManagement" component={MainPage} />
+                <Route path="/cmsUpdate" component={MainPage} />
+                <Route path="/ledger" component={MainPage} />
+                <Route path="/kycApproval" component={MainPage} />
+            </Switch>
+        </>
+    );
 }
 
 function ProtectedRoute({ component: Component, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={props => {
-        if (auth.isAuthenticated()) {
-          return <Component {...props} />;
-        } else {
-          return (
-            <Redirect
-              to={{
-                pathname: "/",
-                state: {
-                  from: props.location
+    return (
+        <Route
+            {...rest}
+            render={props => {
+                if (auth.isAuthenticated()) {
+                    return <Component {...props} />;
+                } else {
+                    return (
+                        <Redirect
+                            to={{
+                                pathname: "/",
+                                state: {
+                                    from: props.location
+                                }
+                            }}
+                        />
+                    );
                 }
-              }}
-            />
-          );
-        }
-      }}
-    />
-  );
+            }}
+        />
+    );
 }
 
 export default App;
